ffa0eb6eb583fd30b1ab4f9025d356a7126b0088
Francisco David Gonzalez Carrillo <frgonzal@c2r4s6.42malaga.com>
2026-09-21 19:44:52 +0200

    prueba

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: frgonzal <frgonzal@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/21 16:41:11 by frgonzal          #+#    #+#             */
-/*   Updated: 2026/09/21 18:10:24 by frgonzal         ###   ########.fr       */
+/*   Updated: 2026/09/21 19:44:40 by frgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ size_t	strlcpy(char *dst, const char *src, size_t size)
 	size_t	i;
 
 	i = 0;
-	if (!(src && dst))
-		return (ft_strlen(src));
+//	if (!(src && dst))
+//		return (ft_strlen(src));
 	while (src[i] && i < size - 1)
 	{
 		dest[i] = src[i];
